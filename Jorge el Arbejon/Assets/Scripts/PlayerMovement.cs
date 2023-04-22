4d6be38d17e29e50354e1f15689f5e4bd4e21388
AaronHChoi <108954995+AaronHChoi@users.noreply.github.com>
2023-04-22 00:28:41 -0300

    Revert "Update PlayerMovement.cs"
    This reverts commit 08de443180b4bcb79263dd4ebc98d89c5fc5882e.

--- Jorge el Arbejon/Assets/Scripts/PlayerMovement.cs
+++ Jorge el Arbejon/Assets/Scripts/PlayerMovement.cs
@@ -11,7 +11,7 @@ public class PlayerMovement : MonoBehaviour
 
     private float moveSpeed = walkSpeed;
 
-    private float jumpingPower = 15f;
+    private float jumpingPower = 20f;
     private bool isFacingRight = true;
 
     private float coyoteTime = 0.2f;
@@ -28,13 +28,6 @@ public class PlayerMovement : MonoBehaviour
 
     private bool isWallSliding;
     private float wallSlidingSpeed = 2f;
-
-    private bool isWallJumping;
-    private float wallJumpingDirection;
-    private float wallJumpingTime = 0.2f;
-    private float wallJumpingCounter;
-    private float wallJumpingDuration = 0.4f;
-    private Vector2 wallJumpingPower = new Vector2(10f, 15f);
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
@@ -88,12 +81,7 @@ public class PlayerMovement : MonoBehaviour
 
         WallSlide();
 
-        WallJump();
-
-        if (!isWallJumping)
-        {
-            Flip();
-        }
+        Flip();
 
         Run();
 
@@ -112,16 +100,12 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (!isWallJumping)
-        {
-            rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
-        }
-
         if (isDashing)
         {
             return;
         }
-       
+
+        rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
     }
 
     private bool IsWalled()
@@ -140,45 +124,6 @@ public class PlayerMovement : MonoBehaviour
         {
             isWallSliding = false;
         }
-    }
-
-    private void WallJump()
-    {
-        if (isWallSliding)
-        {
-            isWallJumping = false;
-            wallJumpingDirection = -transform.localScale.x;
-            wallJumpingCounter = wallJumpingTime;
-
-            CancelInvoke(nameof(StopWallJumping));
-        }
-
-        else
-        {
-            wallJumpingCounter -= Time.deltaTime;
-        }
-
-        if (Input.GetButtonDown("Jump") && wallJumpingCounter > 0f)
-        {
-            isWallJumping = true;
-            rb.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
-            wallJumpingCounter = 0f;
-
-            if (transform.localScale.x != wallJumpingDirection)
-            {
-                isFacingRight = !isFacingRight;
-                Vector3 localScale = transform.localScale;
-                localScale.x *= -1f;
-                transform.localScale = localScale;
-            }
-
-            Invoke(nameof(StopWallJumping), wallJumpingDuration);
-        }
-    }
-
-    private void StopWallJumping()
-    {
-        isWallJumping = false;
     }
 
     private void Flip()
