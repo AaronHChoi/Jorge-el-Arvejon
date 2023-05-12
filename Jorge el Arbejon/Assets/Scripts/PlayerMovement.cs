1c0b45d92bfeacffecf1a8ad13f5b646d429f6d6
Tomas-Lefebvre <lefetomi@gmail.com>
2023-05-12 00:49:55 -0300

    Disparo y cambio de escena

--- Jorge el Arbejon/Assets/Scripts/PlayerMovement.cs
+++ Jorge el Arbejon/Assets/Scripts/PlayerMovement.cs
@@ -195,7 +195,8 @@ public class PlayerMovement : MonoBehaviour
             isFacingRight =  !isFacingRight;
             Vector3 localScale = transform.localScale;
             localScale.x *= -1f;
-            transform.localScale = localScale; 
+            transform.localScale = localScale;
+            
         }
     }
 
