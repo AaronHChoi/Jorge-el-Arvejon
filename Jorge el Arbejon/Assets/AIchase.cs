941a249b4b512567bf12899e3a075ca2554f72b9
AaronHChoi <108954995+AaronHChoi@users.noreply.github.com>
2023-07-19 18:14:25 -0300

    se modifico pa sosicion de los dialogos

--- Jorge el Arbejon/Assets/AIchase.cs
+++ Jorge el Arbejon/Assets/AIchase.cs
@@ -21,12 +21,12 @@ public class AIchase : MonoBehaviour
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
         direction.Normalize();
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         if (distance < 10)
         {
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+            //transform.rotation = Quaternion.Euler(Vector3.forward * angle);
         }
     }
 }
